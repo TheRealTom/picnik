4e53eefc595ecf6fb488392c85b73633944287c9
CZTomasSzabo <tomszabo21@seznam.cz>
2018-12-17 23:31:25 +0100

    oprava skriptu a měnil jsem vše na windows-1250

--- startServer.js
+++ startServer.js
@@ -8,17 +8,10 @@ http.createServer(function (req, res) {
  fs.readFile(filename, function(err, data) {
    if (err) {
      res.writeHead(404, {'Content-Type': 'text/html'});
-     return res.end("404 Not Found");
+     return res.end("404 Not Found, mainPage = localhost:8080/view/mainPage.html");
    }
    res.writeHead(200, {'Content-Type': 'text/html'});
    res.write(data);
    return res.end();
- });
- transporter.sendMail(mailOptions, function(error, info){
-   if (error) {
-     console.log(error);
-   } else {
-     console.log('Email sent: ' + info.response);
-   }
  });
 }).listen(8080);
